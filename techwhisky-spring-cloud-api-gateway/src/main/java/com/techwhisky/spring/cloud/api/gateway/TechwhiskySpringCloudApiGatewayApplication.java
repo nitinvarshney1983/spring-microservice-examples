14891804c78a89718e411cb864d5d39e4891b8a3
nitinvarshney1983 <nitinvarshneykiet@gmail.com>
2023-12-29 14:44:59 +0530

    first commit with api gateway automatic routing

---
+++ techwhisky-spring-cloud-api-gateway/src/main/java/com/techwhisky/spring/cloud/api/gateway/TechwhiskySpringCloudApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.techwhisky.spring.cloud.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechwhiskySpringCloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechwhiskySpringCloudApiGatewayApplication.class, args);
+	}
+
+}
---
+++ techwhisky-spring-cloud-api-gateway/src/test/java/com/techwhisky/spring/cloud/api/gateway/TechwhiskySpringCloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techwhisky.spring.cloud.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechwhiskySpringCloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
