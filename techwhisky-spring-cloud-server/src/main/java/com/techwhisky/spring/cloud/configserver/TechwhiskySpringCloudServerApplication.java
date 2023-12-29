e7a6249c365db695e04f9c005c28715309e365a6
nitinvarshney1983 <nitinvarshneykiet@gmail.com>
2023-12-29 12:26:17 +0530

    added config-repo

---
+++ techwhisky-spring-cloud-server/src/main/java/com/techwhisky/spring/cloud/configserver/TechwhiskySpringCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.techwhisky.spring.cloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechwhiskySpringCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechwhiskySpringCloudServerApplication.class, args);
+	}
+
+}
---
+++ techwhisky-spring-cloud-server/src/test/java/com/techwhisky/spring/cloud/configserver/TechwhiskySpringCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techwhisky.spring.cloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechwhiskySpringCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
